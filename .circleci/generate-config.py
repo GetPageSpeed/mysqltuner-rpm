5e8b04f07a9a4b9e8fe2b4b32c23922cb8d1ddbd
Danila Vershinin <250071+dvershinin@users.noreply.github.com>
2021-07-16 15:50:18 +0300

    Test

--- .circleci/generate-config.py
+++ .circleci/generate-config.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-
 # fetch latest release number for each OS and populate "releases" key in matrix.yml
 # with current and previous release numbers (building for last two major)
-
+import lastversion
 print(lastversion.latest('fedora'))
